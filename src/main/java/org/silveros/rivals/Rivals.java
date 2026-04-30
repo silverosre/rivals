082d2638c54cb117c7181c736d4ec713486b735b
silveros <70025693+silverosre@users.noreply.github.com>
2026-04-30 15:51:21 -0500

    Project setup

---
+++ src/main/java/org/silveros/rivals/Rivals.java
@@ -0,0 +1,16 @@
+package org.silveros.rivals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rivals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
